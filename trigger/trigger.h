19729d751dafdac60b348c7a52060609fc4d251e
sunislentLU <13798401438@163.com>
2017-01-10 17:37:41 +0800

    change trigger io and add  adc enums
    change trigger io and add  adc enums

--- trigger/trigger.h
+++ trigger/trigger.h
@@ -4,17 +4,17 @@
 
 #include<iostm8s105c6.h>
 #define TRG_IN_3  PF_IDR_bit.IDR4 
-#define TRG_IN_4  PB_IDR_bit.IDR4
-#define TRG_IN_1  PA_IDR_bit.IDR1
-#define TRG_IN_2  PA_IDR_bit.IDR2
+//#define TRG_IN_4  PB_IDR_bit.IDR4
+//#define TRG_IN_1  PA_IDR_bit.IDR1
+//#define TRG_IN_2  PA_IDR_bit.IDR2
 
 
 typedef enum
 {
-SW_STATE_IDLE = 0x00,
-SW_STATE_CHG,
-SW_STATE_DOWN
-}_SW_STATE;
+TRG_STATE_IDLE = 0x00,
+TRG_STATE_CHG,
+TRG_STATE_DOWN
+}_TRG_STATUS;
 
 typedef volatile union
 {
@@ -36,7 +36,7 @@ typedef enum
 }_TRG_STATE;
 void Trigger_Init();
 void FollowTest();
-unsigned char   SW_Scan();
+unsigned char   Trg_Scan();
 void ADC_Port_Init();
 void Get_ADC_AnyChannal_Value(unsigned int *AD_Value);
 #endif
