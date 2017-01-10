3b4c626c4bab0359d773e22fc98a14f50c5475dd
sunislentLU <13798401438@163.com>
2017-01-10 17:32:00 +0800

    led flash function add
    add the led flash type enum

--- buzzer_led/buzzer_led.h
+++ buzzer_led/buzzer_led.h
@@ -5,8 +5,10 @@
 #define LED1_ON()   PB_ODR_bit.ODR5 = 0
 #define LED1_OFF()  PB_ODR_bit.ODR5 = 1
 
-#define BUZZER_ON()  PD_ODR_bit.ODR4 = 1
-#define BUZZER_OFF() PD_ODR_bit.ODR4 = 0
+#define BUZZER_ON()   if(BEEP_CSR_bit.BEEPEN == 0)\
+                       BEEP_CSR_bit.BEEPEN = 1
+#define BUZZER_OFF()  if(BEEP_CSR_bit.BEEPEN == 1)\
+                         BEEP_CSR_bit.BEEPEN = 0
 
 typedef enum
 {
@@ -14,8 +16,19 @@ BUZZER_OFF = 0x00,
 BUZZER_ON_SHORT,
 BUZZER_ON_LONG,
 }_BUZZER_TYPE;
-#endif
 
+typedef enum
+{
+  LED_ALWAYS_OFF = 0x00,
+  LED_FLASH_ONE,
+  LED_FLASH_TWO,
+  LED_FLASH_TRIPLE,
+  LED_ALWAYS_ON,
+  LED_TYPE_NONE
+}_LED_TYPE;
 
 void Buzzer_Process();
 void Buzzer_Led_Init();
+void led_Flash_Process();
+
+#endif
