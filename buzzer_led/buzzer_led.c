2c9b694264a0d3e12cb09ecba828eff6095dfdf0
sunislentLU <13798401438@163.com>
2017-01-10 17:31:20 +0800

    buzzer debug
    add  pwm buzzer function and debug the code .
    add led flash control

--- buzzer_led/buzzer_led.c
+++ buzzer_led/buzzer_led.c
@@ -9,7 +9,9 @@ void Buzzer_Led_Init()
   PD_DDR_bit.DDR4 = 1;  //设置端口PD->7的输入输出方向寄存器为输出方向
   PD_CR1_bit.C14  = 1;  //设置为推挽输出
   PD_CR2_bit.C24  = 1;  //设置输出速率为10MHZ
-  BUZZER_OFF();
+  BEEP_CSR_bit.BEEPDIV = 0x02;         //8 * (5 + 2) = 56分频
+  BEEP_CSR_bit.BEEPSEL = 0x00;         //蜂鸣器的输出频率 = Fls / ( 8 * (BEEPDIV + 2) )= 128K / (8 * (5 + 2)) = 2285HZ
+  BEEP_CSR_bit.BEEPEN = 1;
 }
 
 
@@ -24,26 +26,89 @@ void Buzzer_Process()
     break;
   case BUZZER_ON_SHORT:
     BUZZER_ON();
-   buzzer_cnt++; 
-   if(buzzer_cnt == 5)
-   {
-     buzzer_cnt = 0;
-     buzzer_type = BUZZER_OFF;
-   }
+    buzzer_cnt++; 
+    if(buzzer_cnt >= 5)
+    {
+      buzzer_cnt = 0;
+      buzzer_type = BUZZER_OFF;
+    }
   case BUZZER_ON_LONG:
-   BUZZER_ON();
-   buzzer_cnt++; 
-   if(buzzer_cnt == 12)
-   {
-     buzzer_cnt = 0;
-     buzzer_type = BUZZER_OFF;
-   }
+    BUZZER_ON();
+    buzzer_cnt++; 
+    if(buzzer_cnt >= 10)
+    {
+      buzzer_cnt = 0;
+      buzzer_type = BUZZER_OFF;
+    }
     break;
     
   default:
     break;
   }
+}
 
+_LED_TYPE led_flash_type = LED_FLASH_TWO;
 
-
+void led_Flash_Process()
+{
+  static unsigned char led_count = 0; 
+  switch(led_flash_type) 
+  {
+  case LED_ALWAYS_ON:
+    LED1_ON();
+    break;
+  case LED_FLASH_ONE:
+    if(led_count == 0)
+      LED1_ON();
+    led_count++;
+    if(led_count == 5)
+      LED1_OFF();
+    if(led_count == 10)
+      led_count = 0 ;
+    break;
+  case LED_FLASH_TWO:
+    
+    if(led_count == 0)
+      LED1_ON();
+    led_count++;
+    if(led_count == 2)
+      LED1_OFF();
+    if(led_count == 3)
+      LED1_ON();
+    if(led_count == 4)
+      LED1_OFF();
+    if(led_count == 10)
+      led_count = 0;
+   
+    break;  
+  case LED_FLASH_TRIPLE:
+    if(led_count == 0)
+      LED1_ON();
+    led_count++;
+    if(led_count == 2)
+      LED1_OFF();
+    if(led_count == 3)
+      LED1_ON();
+    if(led_count == 4)
+      LED1_OFF();
+    if(led_count == 5)
+      LED1_ON();
+    if(led_count == 6)
+      LED1_OFF();
+    if(led_count == 10)   
+      led_count = 0;
+    
+    break;
+  case LED_ALWAYS_OFF:
+    LED1_OFF();
+    break;
+  case LED_TYPE_NONE:
+    break;
+  default:
+    break;
+  }
+  
+  
+  
+  
 }
